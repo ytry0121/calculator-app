6768a9fd31140e0c4f084f397fbaf50f7424c1e4
Yukiyo Torai <yuki@pedros-mbp.home>
2021-05-19 20:14:43 +0100

    Done to display clicked numbers

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [result, setResult] = useState("");
+
+  function clickedNum(e) {
+    let givenNum = result.replace(/,/g, '') + e.target.innerText;
+    const numFormat = new Intl.NumberFormat('en-GB')
+    setResult(numFormat.format(givenNum))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <h2>calc</h2>
+      <div className="clacWindow">{result}</div>
+      <div className="board">
+        <div className="numBoard">
+          <div className="button" onClick={clickedNum}>1</div>
+          <div className="button" onClick={clickedNum}>2</div>
+          <div className="button" onClick={clickedNum}>3</div>
+          <div className="button" onClick={clickedNum}>4</div>
+          <div className="button" onClick={clickedNum}>5</div>
+          <div className="button" onClick={clickedNum}>6</div>
+          <div className="button" onClick={clickedNum}>7</div>
+          <div className="button" onClick={clickedNum}>8</div>
+          <div className="button" onClick={clickedNum}>9</div>
+        </div>
+        <div className="calcSymbols">
+          <div>+</div>
+          <div>-</div>
+          <div>x</div>
+          <div>/</div>
+        </div>
+      </div>
+      <div onClick={() => setResult("")}>Reset</div>
+      <div>=</div>
     </div>
   );
 }
